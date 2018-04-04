24880b74f5eefe390c54bf32359cd023cb844b9c
Augusto Zeponi <augusto.zeponi@benner.com.br>
2018-04-04 10:34:23 -0300

    #85 - Entendendo os Controllers

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,53 +4,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('contato', function(){
-    return "Página de Contato!";
-});
+Route::get('aluno','Aluno\AlunoController@index');
 
-Route::post('contato', function(){
-    return "Realizando um post";
-});
+Route::get('livro',['uses'=>'LivroController@index','as'=>'livro.index']);
 
-Route::put('contato', function(){
-    return "Realizando um put";
-});
-
-Route::delete('contato', function(){
-    return "Realizando um delete";
-});
-
-Route::match(['get', 'post'], 'sobre',function(){
-    return "Trabalhando com Match";
-});
-
-Route::any('todos',function(){
-    $url = url('nova');
-    return "Todos ".$url;
-});
-
-Route::get('artigo/{id}', function($id){
-    return "Artigo id: {$id}";
-});
-
-Route::get('produto/{id?}', function($id = "marijuana"){
-    return "Produto id: {$id}";
-});
-
-Route::get('produto/{id?}/cor/{cor?}', function($id = "42",$cor = "verde"){
-    return "Produto id: {$id} e Cor = {$cor}";
-});
-
-Route::get('link',['as'=>'link',function(){
-    return 'Link <a href="'.route('detalhes').'">Detalhe</a>';
-}]);
-
-Route::get('teste',['as'=>'detalhes',function(){
-    return "teste";
-}]);
-
-Route::group(['prefix'=>'admin'], function(){
-    Route::get('contato', function(){
-        return "Página de Contato no Admin!";
-    });
-});
+Route::controller('produto','ProdutoController');
