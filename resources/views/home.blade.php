c43f2f6d147fd42488f6caa79df8fc7fa5de1f41
Augusto Zeponi <augusto.zeponi@benner.com.br>
2018-04-11 11:21:39 -0300

    #83 Entendendo o Blade Templates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,13 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Home</title>
-</head>
-<body>
-	<h1>View Home</h1>
-</body>
-</html>
+@extends('layout.principal')
+
+@section('titulo','Home')
+
+@section('navbar')
+	@parent
+	<p>nova navbar</p>
+@endsection
+
+@section('conteudo')
+	<p>Aqui é o conteúdo da Home</p>
+	<p>Usuário: {{ $usuario }}</p>
+@endsection
